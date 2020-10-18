f15ea0a9e0996c0d05723233991d2dfb52bf0449
Dark Tech <cliciosa14@gmail.com>
2020-10-17 21:21:52 -0300

    Updates

---
+++ precache-manifest.2098cd6bc918d31e7ca34fac4f31af26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984a642da5b4f8872e9782d9c9b2eaba",
+    "url": "/borderadius/index.html"
+  },
+  {
+    "revision": "db3a18596cf72a7113f3",
+    "url": "/borderadius/static/css/main.e0fe41e2.chunk.css"
+  },
+  {
+    "revision": "2626ab6d00037fede5cb",
+    "url": "/borderadius/static/js/2.59c56999.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/borderadius/static/js/2.59c56999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3a18596cf72a7113f3",
+    "url": "/borderadius/static/js/main.83912070.chunk.js"
+  },
+  {
+    "revision": "6b086839e45643ed7cb0",
+    "url": "/borderadius/static/js/runtime-main.1237f0e3.js"
+  },
+  {
+    "revision": "3910b68ff80409414cae67971dff4226",
+    "url": "/borderadius/static/media/logo.3910b68f.svg"
+  }
+]);
